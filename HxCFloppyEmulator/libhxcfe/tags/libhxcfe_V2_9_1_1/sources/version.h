c3da0c3e4b55a7a478a66b1a07fcd7ed6de3ca5c
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2018-12-22 23:14:40 +0000

    v2.9.1.1

---
+++ HxCFloppyEmulator/libhxcfe/tags/libhxcfe_V2_9_1_1/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,9,1,1
+#define STR_FILE_VERSION   "2,9,1,1\0"
+#define STR_FILE_VERSION2  "2.9.1.1"
+#define STR_DATE           "18 October 2018\0"
+#define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION

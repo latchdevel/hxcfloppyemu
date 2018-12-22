34c8d0a673fb28b6935ac60cd78bba692fb45a8f
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2018-12-22 23:19:51 +0000

    V2.1.20.1

---
+++ HxCFloppyEmulator/HxCFloppyEmulator_software/tags/HxCFloppyEmulator_software_V2_1_20_1/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,1,20,1
+#define STR_FILE_VERSION   "2,1,20,1\0"
+#define STR_FILE_VERSION2  "2.1.20.1"
+#define STR_DATE           "22 December 2018\0"
+#define NOMFENETRE    "HxCFloppyEmulator v" STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION

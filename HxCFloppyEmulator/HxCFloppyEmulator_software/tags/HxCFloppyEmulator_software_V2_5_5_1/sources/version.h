9b7e87afffcb87e5df9e880145e51f1d72c1e85f
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2020-12-31 18:15:15 +0000

    v2.5.5.1 tag.

---
+++ HxCFloppyEmulator/HxCFloppyEmulator_software/tags/HxCFloppyEmulator_software_V2_5_5_1/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,5,5,1
+#define STR_FILE_VERSION   "2,5,5,1\0"
+#define STR_FILE_VERSION2  "2.5.5.1"
+#define STR_DATE           "31 December 2020\0"
+#define NOMFENETRE    "HxCFloppyEmulator v" STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION
